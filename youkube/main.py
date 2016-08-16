c00db8bdd65116dab77ad4b08da4db88a5541773
lyrl <lyrl@me.com>
2016-08-16 16:00:23 +0800

    init

--- youkube/main.py
+++ youkube/main.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 # Created on 2016-07-26 11:04:34
-import sys
 import os
+import sys
+
 path = os.getcwd()
 if path not in sys.path:
     sys.path.append(path)
 
-import youtube_dl
+import youkube.compoents.youkube_compoent as youkube
 
 class MyLogger(object):
     def debug(self, msg):
@@ -23,21 +24,5 @@ class MyLogger(object):
 
 
 if __name__ == '__main__':
-
-
-    def my_hook(d):
-        print d
-        if d['status'] == 'finished':
-            print('Done downloading, now converting ...')
-
-
-    ydl_opts = {
-        # 'format': 'best/best',
-        # 'logger': MyLogger(),
-        # 'progress_hooks': [my_hook],
-        'proxy': 'socks5://127.0.0.1:1080/',
-        'forcejson': True,
-        'simulate': True
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download(['https://www.youtube.com/watch?v=XUY0-doBx1U'])
+    ykb = youkube.Youkube('config.json')
+    ykb.run()
